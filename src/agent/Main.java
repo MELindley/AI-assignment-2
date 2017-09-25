b6957db0044a929829d2b0558c0564c7edb09da0
Unknown <marco.lindley@hotmail.fr>
2017-09-25 14:53:14 +1000

    Created PathGenerator Class

--- src/agent/Main.java
+++ src/agent/Main.java
@@ -18,7 +18,7 @@ import java.util.Queue;
 import java.util.Stack;
 
 public class Main {
-	static HashSet<Edge>invalidEdges = new HashSet<Edge>();
+	
 	
     public static void main(String args[]) throws IOException {
     	//Load in the problem
@@ -30,8 +30,9 @@ public class Main {
         Graph configSpace = new Graph();
         //intialize the sampler from the spec and the hbvtree
         Sampler sampler = new Sampler(spec,obs,configSpace);
-        //Create edges to see if direct solution is possible 
-        //generateEdges(configSpace,obs);
+        //Create PathGenerator to generate the edges
+        PathGenerator pathGenerator = new PathGenerator(configSpace,obs);
+        pathGenerator.generateEdges();
         //Initialize the searcher
         Search searcher = new Search(configSpace);
         //Loop Sample then Search until we find a solution
@@ -48,8 +49,8 @@ public class Main {
     	//FIFO queue represented by a LinkedList to store the nodes
         LinkedList<HBVNode> nodes = new LinkedList<HBVNode>();
         //Point to store the start and end points of segments, and the line primitive for leaf nodes
-        Point2D.Double start;
-        Point2D.Double end;
+        Point2D.Double start = null;
+        Point2D.Double end = null;
         Line2D.Double primitive;
 	    if(obstacles.size()>0){
 	        for(Obstacle obs : obstacles){
@@ -100,29 +101,4 @@ public class Main {
         return new HBVNode();
     }
     
-    /***
-	 * Checks that a line is valid between two configurations. 
-	 * By Checking every primitive step between them. 
-	 * A Primitive step is valid if: 
-	 * 1. The	length	of	each	broom is	fixed	at	0.05	units	in	length.	
-	 * 2. The	system	must	 form a	connected	chain at	all	 times.
-	 * A connected	chain	means each	ASV	can	be	connected to at	most two brooms and	each end of	each broom is tied to an ASV.
-	 * 3. The polygon  formed by connecting	 the two ends of the connected chain with a straight line segment must,	at	all	times,be convex	and	have an	area of at least	πrmin2,	where	rmin =	0.007(n-1) and n is	the	number	of	ASVs.	
-	 * 4. The brooms must never intersect with each	other.
-5. Brooms	and	ASVs	must	never intersect	with	obstacles.
-6. Brooms	&	ASVs	cannot	move	outside	the	[0,1]X[0,1]	workspace.
-7. The	 planned	 path	 must	 be	 given	 as	 a	 sequence	 of	 positions	 (primitive	
-steps)	such	that	on	each	step,	each	individual	ASV	moves	by	a	distance	of	
-at	most	0.001	units.	
-8. Requirements	1-6 must	hold	at	each	primitive	step.	Since	the	distances	are	
-very	small	(at	most	0.001	unit	length	for	each	ASV),	it	is	sufficient to	test	
-the	requirements	only	at	the	end	of	each	primitive	step.
-	 * @param v1 Start vertex to check
-	 * @param v2 End vertex to check
-	 * @param obs HBVNode of obstacles
-	 * @return True if the line is valid, false otherwise 
-	 */
-//	private boolean checkLineValid(Vertex v1, Vertex v2,HBVNode obs) {
-//		
-//		ArrayList<ASVConfig> primtiveSteps = 
 }
---
+++ src/agent/PathGenerator.java
@@ -0,0 +1,76 @@
+package agent;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+import problem.ASVConfig;
+
+public class PathGenerator {
+	Graph configSpace;
+	HBVNode obstacles;
+	HashSet<Edge>invalidEdges;
+	
+	public PathGenerator(Graph configSpace, HBVNode obstacles){
+		this.configSpace = configSpace;
+		this.obstacles = obstacles;
+		invalidEdges = new HashSet<Edges>();
+	}
+	
+	/***
+    * Helper function to generate edges between vertices of the config space.
+    * @param configspace the config space with to be connected
+    * @param obs HBVNode of obstacles
+    * @return
+    */
+   private  List<Edge> generateEdges(){
+		//initialize result
+		ArrayList<Edge> result = new ArrayList<Edge>();
+		//For each Vertex in the graph
+		for(Vertex v: this.configSpace.getLocations()){
+			for(Vertex v1: this.configSpace.getLocations()){
+				//Check that v != v1 and that the edge is not already in the graphs edges or has already been tested and is invalid
+				Edge toTest = new Edge(v,v1);
+				if(!v.equals(v1)&& !this.configSpace.getEdges().contains(toTest) && ! this.invalidEdges.contains(toTest)){
+						//Check that the line is valid 
+						 if(checkLineValid(v,v1,this.obstacles,-1,-1)){
+							 this.configSpace.addE(toTest);
+							 v.addE(toTest);
+							 v1.addE(toTest);
+						 }else{
+							 //add this edge to the invalid edges
+							 this.invalidEdges.add(toTest);
+						 }
+				}
+				 
+			}
+		}	
+		return result;
+	
+	}
+   /***
+  	 * Checks that a line is valid between two configurations. 
+  	 * By Checking every primitive step between them. 
+  	 * A Primitive step is valid if: 
+  	 * 1. The	length	of	each	broom is	fixed	at	0.05	units	in	length.	
+  	 * 2. The	system	must	 form a	connected	chain at	all	 times.
+  	 * A connected	chain	means each	ASV	can	be	connected to at	most two brooms and	each end of	each broom is tied to an ASV.
+  	 * 3. The polygon  formed by connecting	 the two ends of the connected chain with a straight line segment must,	at	all	times,be convex	and	have an	area of at least	πrmin2,	where	rmin =	0.007(n-1) and n is	the	number	of	ASVs.	
+  	 * 4. The brooms must never intersect with each	other.
+  5. Brooms	and	ASVs	must	never intersect	with	obstacles.
+  6. Brooms	&	ASVs	cannot	move	outside	the	[0,1]X[0,1]	workspace.
+  7. The	 planned	 path	 must	 be	 given	 as	 a	 sequence	 of	 positions	 (primitive	
+  steps)	such	that	on	each	step,	each	individual	ASV	moves	by	a	distance	of	
+  at	most	0.001	units.	
+  8. Requirements	1-6 must	hold	at	each	primitive	step.	Since	the	distances	are	
+  very	small	(at	most	0.001	unit	length	for	each	ASV),	it	is	sufficient to	test	
+  the	requirements	only	at	the	end	of	each	primitive	step.
+  	 * @param v1 Start vertex to check
+  	 * @param v2 End vertex to check
+  	 * @param obs HBVNode of obstacles
+  	 * @return True if the line is valid, false otherwise 
+  	 */
+  	private boolean checkLineValid(Vertex v1, Vertex v2,HBVNode obs) {
+  		
+  		ArrayList<ASVConfig> primtiveSteps =
+}
